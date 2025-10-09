366958d2ce3c48059ec053fab0ec04ee65ea1a0d
Matthew <matthewtoo5@googlemail.com>
2025-10-09 23:09:19 +0100

    Guacamole protocol parser

---
+++ src/guac.h
@@ -0,0 +1,48 @@
+#pragma once
+
+inline QStringList guac_decode(const QString& input)
+{
+	QStringList sections;
+
+	int pos = -1;
+
+	while (true)
+	{
+		qsizetype len = input.indexOf('.', pos + 1);
+
+		if (len == -1)
+			break;
+
+		int length = input.mid(pos + 1, len - pos - 1).toInt();
+
+		pos = length + len + 1;
+
+		// Check if message length is invalid
+		if (pos > input.size())
+			return {};
+
+		sections.push_back(input.mid(len + 1, length));
+
+		QChar sep = (pos < input.size()) ? input[pos] : '\0';
+
+		if (sep == ',')
+			continue;
+		if (sep == ';')
+			break;
+		return {};
+	}
+
+	return sections;
+}
+
+inline QString guac_encode(const QStringList& strings) {
+	QString command;
+
+	for (int i = 0; i < strings.size(); ++i) {
+		const QString& current = strings[i];
+		command += QString::number(current.length()) % '.' % current;
+		command += (i < strings.size() - 1) ? ',' : ';';
+	}
+
+	return command;
+}
